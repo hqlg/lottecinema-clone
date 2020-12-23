0d035ef80862e285c21a7fdf59a7fabeac823d4d
namisan <mongtamquoc2015@gmail.com>
2020-12-23 20:25:30 +0700

    Setup inertialjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,18 @@
 require("./bootstrap");
 
+import { App, plugin } from "@inertiajs/inertia-vue";
 import Vue from "vue";
-import axios from "axios";
-import VueRouter from "vue-router";
-import routes from "./admin/routes";
 
-window.Vue = reuqire("vue");
-Vue.use(vueRouter, axios);
+Vue.use(plugin);
 
-const router = new VueRouter({ mode: "history", routes });
-new Vue(Vue.util.extend({ router })).$mount("#app");
+const el = document.getElementById("app");
+
+new Vue({
+    render: h =>
+        h(App, {
+            props: {
+                initialPage: JSON.parse(el.dataset.page),
+                resolveComponent: path => require(`./${path}`).default
+            }
+        })
+}).$mount(el);
